5db8b50cbc64d3cb78b62b9dd64be51e7752545c
MHShihan <mahmodul.no.1@gmail.com>
2025-01-28 15:20:29 +0600

    Learned basic of python

---
+++ mid/basics-of-python.py
@@ -0,0 +1,14 @@
+name = "Shihan"
+age = 23
+num = 10
+
+def helloworld():
+    print("Hello World")
+
+helloworld()
+
+def printShihan(num, name):
+    for i in range(num):
+        print(i ,"." , name)
+
+printShihan(num, name)
